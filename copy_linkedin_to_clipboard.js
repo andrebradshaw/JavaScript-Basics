6e2daecf1ac619d6835a84a8df5f0fb1b4b3a7ef
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-12-13 23:13:18 -0500

    Update copy_linkedin_to_clipboard.js

--- copy_linkedin_to_clipboard.js
+++ copy_linkedin_to_clipboard.js
@@ -1,35 +1,41 @@
-var reg = (o, n) => o ? o[n] : '';
-
-var fixCase = (s)=> s.split(/\b-\b/).map(el=> el.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('-');
+/*
+    References:
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+        https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByClassName
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+        https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+        https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/select
+        https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+*/
 
 function parseFullName(string){
-  var firstName = reg( /^\S+/.exec(string), 0 );
-  var lastNameChop = string.replace(/^\S+\s+/, '');
-  var lastName = lastNameChop.replace(/\s*[,-]\s*[A-Z\s-,]{3,}/,'');
-  return [fixCase(firstName),fixCase(lastName)];
+    var fixCase = (s)=> s ? s.split(/\b-\b/).map(el=> el.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('-') : s;
+    var first_name = /^\S+/.exec(string) ? /^\S+/.exec(string)[0] : '';
+    var last_name_chop = string.replace(/^\S+\s+/, '').replace(/\s*[,-]\s*[A-Z\s-,]{3,}/,'');
+    return [fixCase(first_name),fixCase(last_name_chop)];
 }
 
 function getLinkedInBasicDetails(){
-  var name_dom_elm = document.getElementsByClassName('inline t-24 t-black t-normal break-words');
-  var full_name = name_dom_elm != undefined && name_dom_elm.length > 0 ? name_dom_elm[0].innerText : null;
-  var profile_link = window.location.href;
-  var firstlast = parseFullName(full_name);
-  return {firstName: firstlast[0], lastName: firstlast[1], url: profile_link};
+    var name_dom_elm = document.getElementsByClassName('inline t-24 t-black t-normal break-words');
+    var full_name = name_dom_elm != undefined && name_dom_elm.length > 0 ? name_dom_elm[0].innerText : null;
+    var profile_link = window.location.href;
+    var firstlast = parseFullName(full_name);
+    return {firstName: firstlast[0], lastName: firstlast[1], url: profile_link};
 }
 
- function copyToClipboard(basicInfo){
-  var tsv_row = basicInfo.firstName + '\t' +basicInfo.lastName + '\t' + basicInfo.url;
-  var tempTextElm = document.createElement('textarea');
-  tempTextElm.value = tsv_row;
-  document.body.appendChild(tempTextElm);
-  tempTextElm.select();
-  document.execCommand('copy');
-  tempTextElm.outerHTML = '';
- }
+function copyToClipboard(basicInfo){
+    var tsv_row = basicInfo.firstName + '\t' +basicInfo.lastName + '\t' + basicInfo.url;
+    var temp_text_elm = document.createElement('textarea');
+    temp_text_elm.value = tsv_row;
+    document.body.appendChild(temp_text_elm);
+    temp_text_elm.select();
+    document.execCommand('copy');
+    temp_text_elm.outerHTML = '';
+}
 
 function copyDetails(){
-  var detail_object = getLinkedInBasicDetails();
-  copyToClipboard(detail_object);
+    var detail_object = getLinkedInBasicDetails();
+    copyToClipboard(detail_object);
 }
 
 copyDetails()
